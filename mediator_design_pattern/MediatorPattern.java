2fcfac7dd5d9fb8bb94959e96ae4a22fa58fbc85
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-09 21:38:17 +0300

    Create MediatorPattern.java

---
+++ mediator_design_pattern/MediatorPattern.java
@@ -0,0 +1,37 @@
+package mediator_design_pattern;
+
+import java.util.Date;
+
+class Chat { 
+public static void showMessage(User user, String message) { 
+    System.out.println(new Date().toString() + "[" + user.getName() + "]: " + message); 
+    } 
+}
+
+class User { 
+    private String name; 
+    public User(String name) { 
+        this.name = name; 
+    } 
+ 
+    public String getName() { 
+        return name; 
+    } 
+    public void setName(String name) { 
+        this.name = name; 
+    } 
+ 
+    public void sendMessage(String message) { 
+        Chat.showMessage(this, message); 
+    } 
+ } 
+
+public class MediatorPattern {
+    public static void main(String[] args) { 
+        User david = new User("David"); 
+        User scott = new User("Scott"); 
+        
+        david.sendMessage("Hi Scott! How are you?"); 
+        scott.sendMessage("I'm great! Thanks for asking. How are you?"); 
+    } 
+}
