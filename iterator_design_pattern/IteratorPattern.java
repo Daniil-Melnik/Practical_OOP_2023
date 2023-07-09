f8672b6cbdc1d46ac26272630de595d1be5401d1
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-09 11:12:35 +0300

    create iterator pattern

---
+++ iterator_design_pattern/IteratorPattern.java
@@ -0,0 +1,52 @@
+package iterator_design_pattern;
+
+interface Iterator { 
+    public boolean hasNext(); 
+    public Object next(); 
+} 
+
+interface Sector { 
+    public Iterator getIterator(); 
+}
+
+class EmployeeRepository implements Sector { 
+    public String[] employees = {"David", "Scott", "Rhett", "Andrew", "Jessica"}; 
+ 
+    @Override 
+    public Iterator getIterator() { 
+        return new EmployeeIterator(); 
+    } 
+    
+    private class EmployeeIterator implements Iterator { 
+    int index; 
+    
+    @Override 
+    public boolean hasNext() { 
+        if(index < employees.length) { 
+            return true; 
+        } 
+        return false; 
+    } 
+    
+    @Override 
+    public Object next() { 
+        if(this.hasNext()) { 
+            return employees[index++]; 
+        } 
+        return null; 
+        } 
+    } 
+} 
+
+public class IteratorPattern {
+    public static void main(String[] args) { 
+ 
+        EmployeeRepository employeeRepository = new EmployeeRepository(); 
+        
+        for(Iterator iterator = employeeRepository.getIterator(); 
+        iterator.hasNext();) { 
+        String employee = (String)iterator.next(); 
+        System.out.println("Employee: " + employee); 
+        } 
+    } 
+} 
